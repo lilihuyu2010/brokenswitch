4fc07072b93f73b961e7e29aa172f468a9d44c49
lilihuyu2010 <lilihuyu2010@163.com>
2017-07-20 10:38:34 +0800

    fixbug

--- b.php
+++ b.php
@@ -1,31 +1,28 @@
 <?php
+session_start();
+//$_SESSION['a'] = 1;
 
+var_dump($_SESSION);die;
 
-$a = '/^\!*?$/';
-$b = '!asdasd';
-if (preg_match($a, $b)) {
-	echo 1;
-} else {
-	echo 2;
-}
-die;
+var_dump("oWnft5mbftXK8VuyqkdZBeJMNh4R0e99GHFmgSWpygou2UN%2Fj7X%2FuKw2GZWtHmeBCcVbL6IOfbCqEA%2FDPENI77N8S9MDt%2Fh9efl%2Fh7g4mlPngzT3Epp2b%2Fq1UbVuk19ZsYM390YU%2FuJ5Q7PXyyabOmXypffKSvEqIfNa%2FoNh2Sk%3D" == "oWnft5mbftXK8VuyqkdZBeJMNh4R0e99GHFmgSWpygou2UN%2Fj7X%2FuKw2GZWtHmeBCcVbL6IOfbCqEA%2FDPENI77N8S9MDt%2Fh9efl%2Fh7g4mlPngzT3Epp2b%2Fq1UbVuk19ZsYM390YU%2FuJ5Q7PXyyabOmXypffKSvEqIfNa%2FoNh2Sk%3D");die;
 
-$file = "/Users/lili/.ssh/id_rsa.pub";
-$source = fopen($file, 'r');
-$key = fread($source, filesize($file));
-
-//$key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7dSLE2Yvx1/klfBZwGALPaJitfjTk86gXk8Sj5wS5i/TnqMhPDJksdkT8fXXlHGUiSiILZefWp+l90NG/gX/AwvuaJBtdtY/6MqvMd1wcV3gcpd1t1JHe9xB2LY+u9iYZ1TxlqcitjgEX8eK3h8UbW/hNNzpjG87JJsfihOyhKup0d1YuFaYiMdJQwQRpXTyFxsOyS+E/neZAA17dUvgaCQPOVTKGdLH/kTHPMoFKn+U/rXZuakpm+sY+dqiuYlVA4BB0ZGwRIiGGxxzMyAULR0bpBtjo/HolqnXUuSqDWTfDbpOe2R8OGJStovgpPvUp7r0YTCV7H32sznWgRSf3 lilihuyu2010@163.com";
-function rsaEncrypt($str,$key)
-{
-    $result  = '';
-    $length = 117;
-    for($i = 0; $i < strlen($str)/$length; $i++  ) {
-        $data = substr($str, $i * $length, $length);
-        openssl_public_encrypt ($data, $encrypt, $key);
-        $result .= $encrypt;
-    }
-
-    return base64_encode($result);
-}
-
-var_dump(rsaEncrypt('abc',$key));
+$dn = array(  
+        "countryName" => 'CN', //所在国家名称  
+        "stateOrProvinceName" => 'Beijing', //所在省份名称  
+        "localityName" => 'Beijing', //所在城市名称  
+        "organizationName" => 'Lili',   //注册人姓名  
+        "organizationalUnitName" => 'yunniao', //组织名称  
+        "commonName" => 'Lili', //公共名称  
+        "emailAddress" => 'lili2010@163.com' //邮箱  
+);  
+    $privkeypass = '111111'; //私钥密码  
+    $numberofdays = 1000;     //有效时长  
+    $cerpath = "/Users/lili/test.cer"; //生成证书路径  
+    $pfxpath = "/Users/lili/test.pfx"; //密钥文件路径  
+    
+    //生成证书  
+    $privkey = openssl_pkey_new(); 
+    $csr = openssl_csr_new($dn, $privkey);  
+    $sscert = openssl_csr_sign($csr, null, $privkey, $numberofdays);  
+    openssl_x509_export_to_file($sscert, $cerpath); //导出证书到文件
+    openssl_pkcs12_export_to_file($sscert, $pfxpath, $privkey, $privkeypass); //生成密钥文件
